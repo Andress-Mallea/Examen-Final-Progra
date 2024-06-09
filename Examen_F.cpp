588045d87745c6e43c029a6718d44ccafb87d79c
Andres Mallea <159574476+Andress-Mallea@users.noreply.github.com>
2024-06-09 07:55:15 -0400

    So

--- Examen_F.cpp
+++ Examen_F.cpp
@@ -7,16 +7,17 @@
 using namespace std;
 int year, month, day;
 int Salida = 0;
-string event;
+string event, date;
 char guio1, guio2;
 map<string, string> events;
+map<string, string> :: iterator it ;
 void add(int año,char guion1, int mes, char guion2, int dia, string evento){
     
     if(mes < 1 || mes > 12){
-        cout << "Month value is invalid" << endl;
+        cout << "Month value is invalid: " + to_string(mes) << endl;
     }
     else if(dia < 1 || dia > 31){
-        cout << "Day value is invalid" << endl;
+        cout << "Day value is invalid: " + to_string(dia) << endl;
     }
     else if(mes >=1 && mes <= 12 && dia >=1 && dia <=31 && guion1 == '-' && guion2 == '-'){
         string año_con_zeros = to_string(año);
@@ -43,14 +44,30 @@ void add(int año,char guion1, int mes, char guion2, int dia, string evento){
 }
 void print(map<string, string> m) {
     for(const auto& item : m) {
+        if(item.second > "0"){
         cout << item.second << " " << item.first << endl;
+        }
+    }
+}
+void Find(string dates){
+    it = events.lower_bound(dates);
+    if(it == events.end()){
+        cout << "Event not found" << endl;
+    }
+    else{
+        cout << it->second << " " << it->first;
+        cout << endl;
     }
 }
 int main() {
     // Write C++ code here
     cout << "Elija un comado a ingresar"<< endl;
-    cout << "1.- Add" << endl;
-    cout << "2.- Print" << endl;
+    cout << "1.- Add Fecha de evento" << endl;
+    cout << "2.- Delete Fecha de evento" << endl;
+    cout << "3.- Delete varias fechas de eventos"<< endl;
+    cout << "4.- Find Fecha de evento" << endl;
+    cout << "5.- Print Mostrar las fechas de eventos" << endl;
+    cout << "6.- Exit" << endl;
     int comando;
     cin >> comando;
     while(comando != 6){
@@ -60,7 +77,12 @@ int main() {
         cin >> year>>guio1>>month>>guio2>>day>>event;
         add(year, guio1, month, guio2, day, event);
         break;
-            case 2:
+        case 4:
+        cout << "Find ";
+        cin >> date;
+        Find(date);
+        break;
+        case 5:
             cout << "Print" << endl;
             print(events);
             break;
